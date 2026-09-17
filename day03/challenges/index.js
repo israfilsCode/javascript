6aa8dc40e32b554b77f871d6e68b020ae56079a6
israfilsCode <israfil.io.code@gmail.com>
2026-09-18 01:28:30 +0600

    learn while loop and solve 2 problems

--- day03/challenges/index.js
+++ day03/challenges/index.js
@@ -11,3 +11,24 @@ for (let i = 1; i <= 20; i++) {
         console.log(i);
     }
 }
+
+
+// 🧪 Challenge 1 — Countdown with while
+
+// let i = 10;
+
+// while (i >= 1) {
+//     console.log(i);
+//     i--;
+// }
+
+// 🧪 Challenge 2 — Even numbers with while
+
+let i = 1;
+
+while(i <= 20) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+    i++;
+}
