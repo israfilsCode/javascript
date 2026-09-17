c22bb30299fd6dc00807f735887cb0e3dd6f8b60
israfilsCode <israfil.io.code@gmail.com>
2026-09-17 21:18:59 +0600

    learn arithmetic operators

---
+++ day01/experiment03.js
@@ -0,0 +1,13 @@
+const a = 20;
+const b = 6;
+
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+console.log("Remainder:", a % b);
+
+const number1 = 5;
+
+// is number1 even?
+console.log(number1 % 2 === 0);
