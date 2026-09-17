56c22074727637c86d7e0ae5f803d6798d4012c1
israfilsCode <israfil.io.code@gmail.com>
2026-09-17 20:11:55 +0600

    learn datatypes in javascript

---
+++ day01/experiment02.js
@@ -0,0 +1,69 @@
+// const name = "Israfil";
+// const age = 20;
+// const isLearning = true;
+// const future = null;
+// let job;
+
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof isLearning);
+// console.log(typeof future);
+// console.log(typeof job);
+
+// string
+// number
+// boolean
+// object
+// undefined
+
+
+// string
+const firstName = "Israfil";
+const lastName = "Mallick";
+
+console.log(firstName + " " + lastName);
+
+
+// number
+const age = 20;
+const price = 99.99;
+const temperature = -5;
+
+console.log(10 + 5);
+console.log(10 - 5);
+console.log(10 * 5);
+console.log(10 / 5);
+
+
+// boolean
+// boolean has only two values true or false
+const isLearning = true;
+console.log(isLearning);
+// This becomes extremely important when we learn conditions.
+
+// For example:
+
+console.log(age >= 18);
+
+
+// null
+// null means intentionally saying that there is no value here.
+const future = null;
+
+let selectedUser = null;
+// There is currently no user selected
+
+console.log(selectedUser);
+// constantly use null when works with real application
+
+
+// undefined
+let job;
+
+console.log(job); // undefined
+
+
+// null -> means intentionally have to value or empty;
+// undefined -> no value has been assigned
+
+console.log(typeof null === "object");
