5c88724161c9e3446bc9f56f2370b94464668ade
israfilsCode <israfil.io.code@gmail.com>
2026-09-17 19:43:05 +0600

    learn javascript variable assignment with const and let

---
+++ day01/experiment01.js
@@ -0,0 +1,12 @@
+// const name = "Israfil";
+let name = "Israfil";
+let age = 20;
+let country = "Bangladesh"
+const learningGoal = "Full stack software engineering";
+
+name = "Dianna";
+
+console.log(name);
+console.log(age);
+console.log(country);
+console.log(learningGoal);
--- day01/index.js
+++ day01/index.js
@@ -1,6 +1,9 @@
 const name ="Israfil";
 let age = 20;
 
+// name ="Alex"; // TypeError: Assignment to constant variable.
+
+console.log(name);
 console.log("Hello, " + name);
 console.log("Age: ", age);
 
