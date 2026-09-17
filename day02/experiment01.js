1b3214c7dd078a8bf129e39f440f8c47324b1c91
israfilsCode <israfil.io.code@gmail.com>
2026-09-17 23:31:44 +0600

    learn comparison operators

---
+++ day02/experiment01.js
@@ -0,0 +1,18 @@
+const age = 20;
+
+// console.log(age > 18);
+// console.log(age < 18);
+// console.log(age >= 18);
+// console.log(age <= 18);
+// console.log(age === 20);
+// console.log(age !== 20);
+
+
+// strict equality and not strict equality
+// Very Important
+console.log(20 === "20"); // false
+console.log(20 == 20); // true
+
+// (===) strict equality check operator checks both type and value
+
+// but (==) performs type conversion before comparing.
