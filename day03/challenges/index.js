9bb57f6d852e10b8d58f93d608a9ab3ee11885ed
israfilsCode <israfil.io.code@gmail.com>
2026-09-18 01:00:25 +0600

    learn for loops in javascript

---
+++ day03/challenges/index.js
@@ -0,0 +1,13 @@
+// Print 10 - 1;
+
+for (let i = 10; i >= 1; i--) {
+    console.log(i);
+}
+
+// Print all even numbers from 1 to 20
+
+for (let i = 1; i <= 20; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
---
+++ day03/experiment01.js
@@ -0,0 +1,5 @@
+// Now we will teach the computer to repeat things
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
