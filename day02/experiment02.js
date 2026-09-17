5c97737bd8ce59b5465760271eaf2f40fade1906
israfilsCode <israfil.io.code@gmail.com>
2026-09-17 23:48:58 +0600

    learn logical operators in javascript

---
+++ day02/experiment02.js
@@ -0,0 +1,36 @@
+// Logical operators
+
+const age = 20;
+const hasId = true;
+
+// console.log(age >= 18 && hasId); // true
+// console.log(age >= 18 || hasId); // true
+// console.log(!hasId); // false
+
+// understand these three
+// console.log(true && true); // true
+// console.log(true && false); // false
+// console.log(false && true); // false
+// console.log(false && false); // false
+
+// && — AND
+// Both conditions must be true.
+
+
+
+// console.log(true || true); // true
+// console.log(true || false); // true
+// console.log(false || true); // true
+// console.log(false || false); // false
+
+// || — OR
+// At least one condition must be true.
+
+
+
+
+console.log(!true); // false
+console.log(!false); // true
+
+// ! — NOT
+// It reverses a boolean.
