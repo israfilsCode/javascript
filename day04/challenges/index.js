c2b0bf3ab586baedff226434212a4d609c082e72
israfilsCode <israfil.io.code@gmail.com>
2026-09-20 22:20:10 +0600

    solve challenges of parameters and arguments in functions

---
+++ day04/challenges/index.js
@@ -0,0 +1,24 @@
+// Challenge 1
+
+function introduce(name, age) {
+    console.log("My name is " + name + " and I am " + age + " years old.");
+}
+
+introduce("Israfil", 20);
+introduce("Alice", 25);
+introduce("Bob", 30);
+
+
+// Challenge 2
+function checkEvenOdd(number) {
+    if (number % 2 === 0) {
+        console.log("Even");
+    } else {
+        console.log("Odd");
+    }
+}
+
+checkEvenOdd(8);
+checkEvenOdd(17);
+checkEvenOdd(24);
+checkEvenOdd(31);
