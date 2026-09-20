7df0243983dbb7018d3d91f89973b7326afcf099
israfilsCode <israfil.io.code@gmail.com>
2026-09-20 22:00:23 +0600

    learn function parameter argument

---
+++ day04/experiment01.js
@@ -0,0 +1,20 @@
+// function checkEvenOdd(number) {
+//     if (number % 2 === 0) {
+//         console.log("Even");
+//     } else {
+//         console.log("Odd");
+//     }
+// }
+
+// checkEvenOdd(8);
+// checkEvenOdd(3);
+// checkEvenOdd(7);
+// checkEvenOdd(10);
+
+function sayHello() {
+    console.log("Hello, World!");
+}
+
+sayHello();
+sayHello();
+sayHello();
---
+++ day04/experiment02.js
@@ -0,0 +1,7 @@
+function greet(name) {
+    console.log("Hello, " + name);
+}
+
+greet("Israfil");
+greet("Alice");
+greet("Bob");
